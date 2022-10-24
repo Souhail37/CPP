9bee3b4a31768920a89d2d856f0e174ae81de02c
Souhail Ismaili alaoui <sismaili@e1r4p7.1337.ma>
2022-10-24 17:59:38 +0100

    fixing some problems

--- module_00/ex01/searchContact.cpp
+++ module_00/ex01/searchContact.cpp
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/23 11:37:07 by sismaili          #+#    #+#             */
-/*   Updated: 2022/10/24 16:07:24 by sismaili         ###   ########.fr       */
+/*   Updated: 2022/10/24 17:54:25 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	PhoneBook::search_fn(int n)
 {
-	std::cout << std::setw(10) << n << " ";
+	std::cout << std::setw(10) << n << "|";
 	if (contacts[n].getfirst_name().length() > 10)
 	{
 		std::string	str = contacts[n].getfirst_name().substr(0, 10);
 		str.back() = '.';
-		std::cout << std::setw(10) << str << " ";
+		std::cout << std::setw(10) << str << "|";
 	}
 	else
-		std::cout << std::setw(10) << contacts[n].getfirst_name() << " ";
+		std::cout << std::setw(10) << contacts[n].getfirst_name() << "|";
 }
 
 void	PhoneBook::search_ln(int n)
@@ -31,10 +31,10 @@ void	PhoneBook::search_ln(int n)
 	{
 		std::string	str = contacts[n].getlast_name().substr(0, 10);
 		str.back() = '.';
-		std::cout << std::setw(10) << str << " ";
+		std::cout << std::setw(10) << str << "|";
 	}
 	else
-		std::cout << std::setw(10) << contacts[n].getlast_name() << " ";
+		std::cout << std::setw(10) << contacts[n].getlast_name() << "|";
 }
 
 void	PhoneBook::search_nn(int n)
@@ -43,31 +43,31 @@ void	PhoneBook::search_nn(int n)
 	{
 		std::string	str = contacts[n].getnickname().substr(0, 10);
 		str.back() = '.';
-		std::cout << std::setw(10) << str;
+		std::cout << std::setw(10) << str << "|";
 	}
 	else
-		std::cout << std::setw(10) << contacts[n].getnickname();
+		std::cout << std::setw(10) << contacts[n].getnickname() << "|";
 }
 
 void	PhoneBook::contact_info()
 {
 	std::string	index;
-	int			i = 0;
+	int			i = -1;
 
-	while (index.empty() || (i <= 1 || i >= 8))
+	while (index.empty() || (i <= 0 || i >= 7))
 	{
 		std::cout << "Write an index to display : ";
 		if (!std::getline(std::cin, index))
 			exit (1);
 		i = atoi(index.c_str());
-		if (i >= 1 && i <= 8)
+		if (i >= 0 && i <= 7)
 		{
 			std::cout << "index is " << i << "\n";
-			std::cout << "first name : " << contacts[i - 1].getfirst_name() << std::endl;
-			std::cout << "last name : " << contacts[i - 1].getlast_name() << std::endl;
-			std::cout << "nick name : " << contacts[i - 1].getnickname() << std::endl;
-			std::cout << "phone number : " << contacts[i - 1].getphonenumber() << std::endl;
-			std::cout << "darkest secret : " << contacts[i - 1].getdarkest_secret() << std::endl;
+			std::cout << "first name : " << contacts[i].getfirst_name() << std::endl;
+			std::cout << "last name : " << contacts[i].getlast_name() << std::endl;
+			std::cout << "nick name : " << contacts[i].getnickname() << std::endl;
+			std::cout << "phone number : " << contacts[i].getphonenumber() << std::endl;
+			std::cout << "darkest secret : " << contacts[i].getdarkest_secret() << std::endl;
 			break;
 		}
 		else
@@ -79,17 +79,25 @@ void	PhoneBook::search()
 {
 	int	n = 0;
 
-	std::cout << std::setw(10) << "index" << "|"
-	<< std::setw(10) << "first name" << "|"
-	<< std::setw(10) << "last name" << "|"
-	<< std::setw(10) << "nickname" << "|" << std::endl;
-	while (n < 8)
+	if (contacts[0].getfirst_name().empty())
+		std::cout << "THERE IS NO CONTACT YET \n";
+	else
 	{
-		PhoneBook::search_fn(n);
-		PhoneBook::search_ln(n);
-		PhoneBook::search_nn(n);
-		n++;
-		std::cout << std::endl;
+		std::cout << std::setw(10) << "index" << "|"
+		<< std::setw(10) << "first name" << "|"
+		<< std::setw(10) << "last name" << "|"
+		<< std::setw(10) << "nickname" << "|" << std::endl;
+		while (n < 8)
+		{
+			if (!contacts[n].getfirst_name().empty())
+			{
+				PhoneBook::search_fn(n);
+				PhoneBook::search_ln(n);
+				PhoneBook::search_nn(n);
+				std::cout << std::endl;
+			}
+			n++;
+		}
+		PhoneBook::contact_info();
 	}
-	PhoneBook::contact_info();
 }
