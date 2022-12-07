8e6009627a3ebed2a4bd08198d95e031af55ee25
Souhail Ismaili alaoui <sismaili@e1r4p15.1337.ma>
2022-12-07 17:17:57 +0100

    modified

--- module_07/ex02/main.cpp
+++ module_07/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 00:59:33 by sismaili          #+#    #+#             */
-/*   Updated: 2022/12/07 15:42:40 by sismaili         ###   ########.fr       */
+/*   Updated: 2022/12/07 16:57:28 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main()
 		Array<int>	arr(5);
 		
 		std::cout << "arr = ";
-		for (unsigned int i = 0; i < arr.size(); i++)
+		for (int i = 0; i < arr.size(); i++)
 		{
 			arr[i] = i + 1;
 			std::cout << arr[i] << " ";
@@ -28,20 +28,20 @@ int main()
 
 		std::cout << std::endl;
 		std::cout << "arr2 = ";
-		for (unsigned int i = 0; i < arr2.size(); i++)
+		for (int i = 0; i < arr2.size(); i++)
 			std::cout << arr2[i] << " ";
 
-		for (unsigned int i = 0; i < arr.size(); i++)
+		for (int i = 0; i < arr.size(); i++)
 			arr[i] = i * i;
 		
 		std::cout << std::endl;
 		std::cout << "arr = ";
-		for (unsigned int i = 0; i < arr.size(); i++)
+		for (int i = 0; i < arr.size(); i++)
 			std::cout << arr[i] << " ";
 		
 		std::cout << std::endl;
 		std::cout << "arr2 = ";
-		for (unsigned int i = 0; i < arr2.size(); i++)
+		for (int i = 0; i < arr2.size(); i++)
 			std::cout << arr2[i] << " ";
 	}
 	catch (std::exception &err)
