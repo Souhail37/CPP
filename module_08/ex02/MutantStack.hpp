eeaffe34c445ea1b762655a378ac1b5cf95f21ef
Souhail Ismaili alaoui <sismaili@e1r1p4.1337.ma>
2022-12-08 23:50:11 +0100

    modified

--- module_08/ex02/MutantStack.hpp
+++ module_08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/08 17:37:03 by sismaili          #+#    #+#             */
-/*   Updated: 2022/12/08 22:55:47 by sismaili         ###   ########.fr       */
+/*   Updated: 2022/12/08 23:42:16 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,24 @@
 #include <stack>
 #include <deque>
 
-template<typename T, typename container = std::deque<T> >
+template<typename T >
 class   MutantStack : public std::stack<T>
 {
 	public :
-		typedef typename container::iterator iterator;
+		typedef typename MutantStack<T>::container_type::iterator iterator;
+		MutantStack()
+		{}
+		MutantStack(const MutantStack &mt)
+		{
+			*this = mt;
+		}
+		MutantStack	&operator=(const MutantStack &mt)
+		{
+			this->c = mt.c;
+			return *this;
+		}
+		~MutantStack()
+		{}
 		iterator	begin()
 		{
 			return this->c.begin();
