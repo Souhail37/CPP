0b9a85e26fd26b66bd9473b873b67d362da180ab
Souhail37 <i.alaoui.souhail@gmail.com>
2023-05-05 23:10:18 +0100

    the merge-insert algorithm is done

--- module_09/ex02/PmergeMe.cpp
+++ module_09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 23:18:10 by sismaili          #+#    #+#             */
-/*   Updated: 2023/05/05 18:39:05 by sismaili         ###   ########.fr       */
+/*   Updated: 2023/05/05 22:48:22 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,7 @@ void	PmergeMe::insertion_vector(std::vector<int> &v_sort)
 	for (std::size_t i = 1; i < v_sort.size(); i++)
 	{
 		int	temp = v_sort[i];
-		std::size_t	j = i - 1;
+		int	j = i - 1;
 		while (j >= 0 && v_sort[j] > temp)
 		{
 			v_sort[j + 1] = v_sort[j];
@@ -53,15 +53,140 @@ void	PmergeMe::insertion_deque(std::deque<int> &d_sort)
 	for (std::size_t i = 1; i < d_sort.size(); i++)
 	{
 		int	temp = d_sort[i];
-		std::size_t	j = i - 1;
+		int	j = i - 1;
 		while (j >= 0 && d_sort[j] > temp)
 		{
 			d_sort[j + 1] = d_sort[j];
-		std::cout << "hello" << std::endl;
 			j--;
 		}
 		d_sort[j + 1] = temp;
 	}
+}
+
+void	PmergeMe::merge(std::deque<int> &left, std::deque<int> &right, std::deque<int> &d_sort)
+{
+	int	left_size = d_sort.size() / 2;
+	int	right_size = d_sort.size() - left_size;
+	int	i = 0, l = 0, r = 0;
+
+	while (l < left_size && r < right_size)
+	{
+		if (left[l] < right[r])
+		{
+			d_sort[i] = left[l];
+			i++;
+			l++;
+		}
+		else
+		{
+			d_sort[i] = right[r];
+			i++;
+			r++;
+		}
+	}
+	while (l < left_size)
+	{
+		d_sort[i] = left[l];
+		i++;
+		l++;
+	}
+	while (r < right_size)
+	{
+		d_sort[i] = right[r];
+		i++;
+		r++;
+	}
+}
+
+void	PmergeMe::merge_deque(std::deque<int> &d_sort)
+{
+	int	length = d_sort.size();
+	if (length <= 15)
+	{
+		insertion_deque(d_sort);	
+		return ;
+	}
+	int	middle = length / 2;
+	std::deque<int> left;
+	std::deque<int> right;
+	int	j = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (i < middle)
+			left.push_back(d_sort[i]);
+		else
+		{
+			right.push_back(d_sort[i]);
+			j++;
+		}
+	}
+	merge_deque(left);
+	merge_deque(right);
+	merge(left, right, d_sort);
+}
+
+void	PmergeMe::merge(std::vector<int> &left, std::vector<int> &right, std::vector<int> &v_sort)
+{
+	int	left_size = v_sort.size() / 2;
+	int	right_size = v_sort.size() - left_size;
+	int	i = 0, l = 0, r = 0;
+
+	while (l < left_size && r < right_size)
+	{
+		if (left[l] < right[r])
+		{
+			v_sort[i] = left[l];
+			i++;
+			l++;
+		}
+		else
+		{
+			v_sort[i] = right[r];
+			i++;
+			r++;
+		}
+	}
+	while (l < left_size)
+	{
+		v_sort[i] = left[l];
+		i++;
+		l++;
+	}
+	while (r < right_size)
+	{
+		v_sort[i] = right[r];
+		i++;
+		r++;
+	}
+}
+
+void	PmergeMe::merge_vector(std::vector<int> &v_sort)
+{
+	int	length = v_sort.size();
+	if (length <= 15)
+	{
+		insertion_vector(v_sort);	
+		return ;
+	}
+	int	middle = length / 2;
+	std::vector<int> left;
+	std::vector<int> right;
+	int	j = 0;
+
+	for (int i = 0; i < length; i++)
+	{
+		if (i < middle)
+			left.push_back(v_sort[i]);
+		else
+		{
+			right.push_back(v_sort[i]);
+			j++;
+		}
+	}
+	merge_vector(left);
+	merge_vector(right);
+	merge(left, right, v_sort);
 }
 
 PmergeMe::PmergeMe(std::vector<std::string> &temp)
@@ -86,6 +211,15 @@ PmergeMe::PmergeMe(std::vector<std::string> &temp)
 		v_end = (double)(clock() - v_time) / CLOCKS_PER_SEC;
 		d_time = clock();
 		insertion_deque(d_sort);
+		d_end = (double)(clock() - d_time) / CLOCKS_PER_SEC;
+	}
+	else
+	{
+		v_time = clock();
+		merge_vector(v_sort);
+		v_end = (double)(clock() - v_time) / CLOCKS_PER_SEC;
+		d_time = clock();
+		merge_deque(d_sort);
 		d_end = (double)(clock() - d_time) / CLOCKS_PER_SEC;
 	}
 	std::cout << "After:";
--- module_09/ex02/PmergeMe.hpp
+++ module_09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: sismaili <sismaili@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 23:14:34 by sismaili          #+#    #+#             */
-/*   Updated: 2023/05/05 18:35:49 by sismaili         ###   ########.fr       */
+/*   Updated: 2023/05/05 22:32:48 by sismaili         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ class PmergeMe
 	private:
 		std::vector<int> v_sort;
 		std::deque<int> d_sort;
-		// int	n;
 	public:
 		PmergeMe();
 		PmergeMe(const PmergeMe &copy);
@@ -32,6 +31,10 @@ class PmergeMe
 		PmergeMe(std::vector<std::string> &temp);
 		void	insertion_vector(std::vector<int> &v_sort);
 		void	insertion_deque(std::deque<int> &d_sort);
+		void	merge_vector(std::vector<int> &v_sort);
+		void	merge_deque(std::deque<int> &d_sort);
+		void	merge(std::vector<int> &left, std::vector<int> &right, std::vector<int> &v_sort);
+		void	merge(std::deque<int> &left, std::deque<int> &right, std::deque<int> &d_sort);
 };
 
 #endif
